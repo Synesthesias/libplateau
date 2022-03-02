2aaac0fe088eec3460c66786239883ed98d1e177
Wataru Yasuda <wataru.robo@gmail.com>
2022-03-02 21:54:29 +0900

    output .obj file with texture

--- include/obj_writer.h
+++ include/obj_writer.h
@@ -13,7 +13,8 @@ public:
 
 private:
     unsigned int writeVertices(const std::vector<TVec3d>& vertices);
-    void writeIndices(const std::vector<unsigned int>& indices, unsigned int ix_offset);
+    void writeIndices(const std::vector<unsigned int>& indices, unsigned int ix_offset, unsigned int tx_offset, bool tex_flg);
+    unsigned int writeUVs(const std::vector<TVec2f>& uvs);
 
     std::ofstream ofs_;
 };
--- src/io/obj_writer.cpp
+++ src/io/obj_writer.cpp
@@ -1,4 +1,4 @@
-#include <stdexcept>
+﻿#include <stdexcept>
 #include <vector>
 #include <algorithm>
 #include <iomanip>
@@ -7,13 +7,19 @@
 #include <citygml/citymodel.h>
 #include <citygml/geometry.h>
 #include <citygml/polygon.h>
+#include <citygml/texture.h>
 
 #include "obj_writer.h"
 #include "polar_to_plane_cartesian.h"
 
 void ObjWriter::write(const std::string& file_path, const citygml::CityModel& city_model) {
 
-    unsigned int v_cnt = 0, cnt;
+    unsigned int v_cnt = 0, t_cnt = 0, cntv, cntt;
+    std::vector<std::string> mat_list;
+    std::ofstream ofs_mat("materials.mtl");
+    if (!ofs_mat.is_open()) {
+        throw std::runtime_error("Failed to open materials.mtl");
+    }
 
     ofs_ = std::ofstream(file_path);
     if (!ofs_.is_open()) {
@@ -23,6 +29,7 @@ void ObjWriter::write(const std::string& file_path, const citygml::CityModel& ci
     ofs_ << std::fixed << std::setprecision(6);
     const auto rc = city_model.getNumRootCityObjects();
     std::cout << "NumRootCityObjects : " << rc << std::endl;
+    ofs_ << "mtllib materials.mtl" << std::endl;
     for (unsigned int h = 0; h < rc; h++) {
         std::cout << "建物ID : " << city_model.getRootCityObject(h).getAttribute("建物ID") << std::endl;
         const auto cc = city_model.getRootCityObject(h).getChildCityObjectsCount();
@@ -35,9 +42,37 @@ void ObjWriter::write(const std::string& file_path, const citygml::CityModel& ci
             for (unsigned int j = 0; j < gc; j++) {
                 const auto pc = target_object.getGeometry(j).getPolygonsCount();
                 for (unsigned int k = 0; k < pc; k++) {
-                    cnt = writeVertices(target_object.getGeometry(j).getPolygon(k)->getVertices());
-                    writeIndices(target_object.getGeometry(j).getPolygon(k)->getIndices(), v_cnt);
-                    v_cnt += cnt;
+                    cntv = writeVertices(target_object.getGeometry(j).getPolygon(k)->getVertices());
+
+                    const auto citygmlTex = target_object.getGeometry(j).getPolygon(k)->getTextureFor("rgbTexture");
+                    bool tex_flg = false;
+                    cntt = 0;
+                    if(citygmlTex){ 
+                        tex_flg = true;
+                        std::string tex_path = citygmlTex->getUrl();
+                        size_t path_i = tex_path.find_last_of("/") + 1;
+                        size_t ext_i = tex_path.find_last_of(".");
+                        std::string mat_name = tex_path.substr(path_i, ext_i - path_i);
+                        cntt = writeUVs(target_object.getGeometry(j).getPolygon(k)->getTexCoordsForTheme("rgbTexture", true));
+                        ofs_ << "usemtl " << mat_name << std::endl;
+                        
+                        bool newmat_flg = true;
+                        for (const auto& item : mat_list) {
+                            if (item == mat_name) {
+                                newmat_flg = false;
+                                break;
+                            }
+                        }
+                        if (newmat_flg) {
+                            ofs_mat << "newmtl " << mat_name << std::endl;
+                            ofs_mat << "map_Kd ./" << tex_path << std::endl << std::endl;
+                            mat_list.push_back(mat_name);
+                        }
+                    }
+                    
+                    writeIndices(target_object.getGeometry(j).getPolygon(k)->getIndices(), v_cnt, t_cnt, tex_flg);
+                    v_cnt += cntv;
+                    t_cnt += cntt;
                 }
             }
         }
@@ -57,12 +92,25 @@ unsigned int ObjWriter::writeVertices(const std::vector<TVec3d>& vertices) {
     return cnt;
 }
 
-void ObjWriter::writeIndices(const std::vector<unsigned int>& indices, unsigned int ix_offset) {
-    int i = 0;
+unsigned int ObjWriter::writeUVs(const std::vector<TVec2f>& uvs) {
+    int cnt = 0;
+    std::for_each(uvs.cbegin(), uvs.cend(), [&](const TVec2f& uv) {
+        ofs_ << "vt " << uv.x << " " << uv.y << std::endl;
+        cnt++;
+    });
+    return cnt;
+}
 
+void ObjWriter::writeIndices(const std::vector<unsigned int>& indices, unsigned int ix_offset, unsigned int tx_offset, bool tex_flg) {
+    int i = 0;
     for (auto itr = indices.begin(); itr != indices.end(); itr++) {
         if (i == 0) ofs_ << "f ";
-        ofs_ << *itr + 1 + ix_offset << " ";
+        if (tex_flg) {
+            ofs_ << *itr + 1 + ix_offset << "/" << *itr + 1 + tx_offset << " ";
+        }
+        else {
+            ofs_ << *itr + 1 + ix_offset << " ";
+        }
         i++;
         if (i == 3) {
             i = 0;
