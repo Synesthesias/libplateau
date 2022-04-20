65ab9325cebb573de61fcee818be4dad8319f47b
linoal <1321932+linoal@users.noreply.github.com>
2022-04-20 23:16:50 +0900

    同上

--- wrappers/csharp/LibPLATEAU.NET/LibPLATEAU.NET.Test/AttributesDictionaryTests.cs
+++ wrappers/csharp/LibPLATEAU.NET/LibPLATEAU.NET.Test/AttributesDictionaryTests.cs
@@ -16,42 +16,42 @@ namespace LibPLATEAU.NET.Test
             var cityObject = cityModel.RootCityObjects[0];
             this.attrDict = cityObject.AttributesDictionary;
         }
-        //
-        // [TestMethod]
-        // public void Test_GetKeys()
-        // {
-        //     var keys = this.attrDict.Keys.ToArray();
-        //     bool doContainKey = false;
-        //     foreach (var k in keys)
-        //     {
-        //         Console.WriteLine($"{k}\n");
-        //         if (k == "多摩水系多摩川、浅川、大栗川洪水浸水想定区域（想定最大規模）")
-        //         {
-        //             doContainKey = true;
-        //         }
-        //     }
-        //     Assert.IsTrue(doContainKey);
-        //     Assert.AreEqual(this.attrDict.Count, keys.Length);
-        // }
-        //
-        //
-        // [TestMethod]
-        // public void Test_Count()
-        // {
-        //     Console.WriteLine($"Count = {this.attrDict.Count}");
-        //     Assert.IsTrue(this.attrDict.Count > 0);
-        // }
-        //
-        // [TestMethod]
-        // public void Test_GetAttributeValueAsString()
-        // {
-        //     var val1 = this.attrDict["建物ID"];
-        //     Assert.AreEqual("13111-bldg-147301", val1.AsString);
-        //     var val2 = this.attrDict["bldg:measuredheight"];
-        //     
-        //     Assert.AreEqual("2.8（テスト）", val2.AsString);
-        // }
-        //
+        
+        [TestMethod]
+        public void Test_GetKeys()
+        {
+            var keys = this.attrDict.Keys.ToArray();
+            bool doContainKey = false;
+            foreach (var k in keys)
+            {
+                Console.WriteLine($"{k}\n");
+                if (k == "多摩水系多摩川、浅川、大栗川洪水浸水想定区域（想定最大規模）")
+                {
+                    doContainKey = true;
+                }
+            }
+            Assert.IsTrue(doContainKey);
+            Assert.AreEqual(this.attrDict.Count, keys.Length);
+        }
+        
+        
+        [TestMethod]
+        public void Test_Count()
+        {
+            Console.WriteLine($"Count = {this.attrDict.Count}");
+            Assert.IsTrue(this.attrDict.Count > 0);
+        }
+        
+        [TestMethod]
+        public void Test_GetAttributeValueAsString()
+        {
+            var val1 = this.attrDict["建物ID"];
+            Assert.AreEqual("13111-bldg-147301", val1.AsString);
+            var val2 = this.attrDict["bldg:measuredheight"];
+            
+            Assert.AreEqual("2.8（テスト）", val2.AsString);
+        }
+        
         [TestMethod]
         public void Test_GetAttributeType()
         {
@@ -63,19 +63,19 @@ namespace LibPLATEAU.NET.Test
             Assert.AreEqual(AttributeType.AttributeSet, val3.Type);
         }
         
-        // [TestMethod]
-        // public void Test_ChildAttributesMap()
-        // {
-        //     var parent = this.attrDict["多摩水系多摩川、浅川、大栗川洪水浸水想定区域（想定最大規模）"];
-        //     var children = parent.AsAttrSet;
-        //     // 参考用にキーの一覧を表示します。
-        //     Console.WriteLine($"parent: {this.attrDict}");
-        //     Console.WriteLine($"children: {children}");
-        //
-        //     var cval = children["規模"];
-        //     Console.WriteLine($"child value => {cval.AsString}");
-        //     Assert.AreEqual(cval.AsString, "L2");
-        // }
+        [TestMethod]
+        public void Test_ChildAttributesMap()
+        {
+            var parent = this.attrDict["多摩水系多摩川、浅川、大栗川洪水浸水想定区域（想定最大規模）"];
+            var children = parent.AsAttrSet;
+            // 参考用にキーの一覧を表示します。
+            Console.WriteLine($"parent: {this.attrDict}");
+            Console.WriteLine($"children: {children}");
+        
+            var cval = children["規模"];
+            Console.WriteLine($"child value => {cval.AsString}");
+            Assert.AreEqual(cval.AsString, "L2");
+        }
 
 
         [TestMethod]
@@ -98,54 +98,54 @@ namespace LibPLATEAU.NET.Test
             Assert.AreEqual(true, result2);
         }
         
-        [TestMethod]
-        public void Test_Values()
-        {
-            var values = this.attrDict.Values.ToArray();
-            foreach (var val in values)
-            {
-                Console.WriteLine(val.AsString);
-            }
-            Assert.AreEqual(true, values.Select(v => v.AsString).Contains("13111-bldg-147301"));
-            Assert.AreEqual(this.attrDict.Count, values.Length);
-        }
-        //
-        // /// <summary>
-        // /// テスト内容 : foreachを使って Dictionary の全要素を回すことができます。
-        // /// </summary>
         // [TestMethod]
-        // public void Test_Enumerable()
+        // public void Test_Values()
         // {
-        //     int count = 0;
-        //     foreach (var p in this.attrDict)
+        //     var values = this.attrDict.Values.ToArray();
+        //     foreach (var val in values)
         //     {
-        //         Console.WriteLine($"{p.Key}, {p.Value.AsString}");
-        //         count++;
+        //         Console.WriteLine(val.AsString);
         //     }
-        //     Assert.AreEqual(this.attrDict.Count, count);
+        //     Assert.AreEqual(true, values.Select(v => v.AsString).Contains("13111-bldg-147301"));
+        //     Assert.AreEqual(this.attrDict.Count, values.Length);
         // }
-        //
-        // /// <summary>
-        // /// テスト内容 : 存在しないキーを参照しようとしたとき、KeyNotFoundException を投げます。
-        // /// </summary>
-        // [TestMethod]
-        // [ExpectedException(typeof(KeyNotFoundException))]
-        // public void Test_MissingKey()
-        // {
-        //     var _ = this.attrDict["DummyNotFound"];
-        // }
-        //
-        // [TestMethod]
-        // public void Test_AsDouble()
-        // {
-        //     Assert.AreEqual(123.456, this.attrDict["doubleAttributeテスト"].AsDouble);
-        // }
-        //
-        // [TestMethod]
-        // public void Test_AsInt()
-        // {
-        //     Assert.AreEqual(123, this.attrDict["intAttributeテスト"].AsInt);
-        // }
+        
+        /// <summary>
+        /// テスト内容 : foreachを使って Dictionary の全要素を回すことができます。
+        /// </summary>
+        [TestMethod]
+        public void Test_Enumerable()
+        {
+            int count = 0;
+            foreach (var p in this.attrDict)
+            {
+                Console.WriteLine($"{p.Key}, {p.Value.AsString}");
+                count++;
+            }
+            Assert.AreEqual(this.attrDict.Count, count);
+        }
+        
+        /// <summary>
+        /// テスト内容 : 存在しないキーを参照しようとしたとき、KeyNotFoundException を投げます。
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void Test_MissingKey()
+        {
+            var _ = this.attrDict["DummyNotFound"];
+        }
+        
+        [TestMethod]
+        public void Test_AsDouble()
+        {
+            Assert.AreEqual(123.456, this.attrDict["doubleAttributeテスト"].AsDouble);
+        }
+        
+        [TestMethod]
+        public void Test_AsInt()
+        {
+            Assert.AreEqual(123, this.attrDict["intAttributeテスト"].AsInt);
+        }
         
     }
 }
