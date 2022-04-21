da518d8a4b60cb30492c69d5069a82f28b706585
linoal <1321932+linoal@users.noreply.github.com>
2022-04-21 19:01:33 +0900

    Object.Id でIdを取得できることのテストを追記

--- wrappers/csharp/LibPLATEAU.NET/LibPLATEAU.NET.Test/ObjectTests.cs
+++ wrappers/csharp/LibPLATEAU.NET/LibPLATEAU.NET.Test/ObjectTests.cs
@@ -14,5 +14,11 @@ namespace LibPLATEAU.NET.Test
             var cityModel = TestGMLLoader.LoadTestGMLFile();
             plateauObject = cityModel.RootCityObjects[0];
         }
+
+        [TestMethod]
+        public void Test_Get_Id()
+        {
+            Assert.AreEqual(this.plateauObject.ID, "BLD_0772bfd9-fa36-4747-ad0f-1e57f883f745");
+        }
     }
 }
