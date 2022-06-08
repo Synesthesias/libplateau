39ab80729644bb6e931dad27be1fe7be46ae2b60
和正 崎山 <kazsakinotwen@gmail.com>
2022-06-08 21:06:13 +0900

    CI対応

--- test/test_obj_writer.cpp
+++ test/test_obj_writer.cpp
@@ -5,17 +5,28 @@
 #include <citygml/citygml.h>
 #include <citygml/citymodel.h>
 
-TEST(ObjWriter, OutputsObj) {
+namespace {
+    void assertFileExists(const std::string& file_path)
+    {
+        std::ifstream ifs(file_path);
+        ASSERT_TRUE(ifs.is_open());
+        ifs.close();
+    }
+}
+
+TEST(ObjWriter, OutputsObjAndMtl) {
     const auto path = "../data/udx/bldg/53392642_bldg_6697_op2.gml";
     const auto output_path = "output.obj";
-    ObjWriter obj_writer;
-    const citygml::ParserParams parser_params;
-    const auto city_model = citygml::load(path, parser_params, nullptr);
-    obj_writer.write(output_path, *city_model, path);
-
-    std::ifstream ifs(output_path);
-    EXPECT_TRUE(ifs.is_open());
-    ifs.close();
+    const auto mtl_path = std::filesystem::path(output_path).replace_extension("mtl").string();
 
     std::filesystem::remove(output_path);
+    std::filesystem::remove(mtl_path);
+
+    const citygml::ParserParams parser_params;
+    const auto city_model = citygml::load(path, parser_params, nullptr);
+
+    ObjWriter().write(output_path, *city_model, path);
+
+    assertFileExists(output_path);
+    assertFileExists(mtl_path);
 }
