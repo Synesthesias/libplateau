fea5dfa123c06cf88e8f0173f162e9c48527d1da
Linoal <1321932+linoal@users.noreply.github.com>
2022-06-22 18:32:45 +0900

    C#の名前空間を変更 (#44)
    * 名前空間変更
    
    * README追記
    
    * CI修正
    
    * 同上:
    
    * 同上
    
    * ObjWriterの名前空間を変更

---
+++ wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/CityGML/LinearRingTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PLATEAU.CityGML;
+
+namespace PLATEAU.Test.CityGML
+{
+    [TestClass]
+    public class LinearRingTests
+    {
+        private readonly LinearRing exteriorRing;
+        
+        // 前処理
+        public LinearRingTests()
+        {
+            CityModel cityModel = TestUtil.LoadTestGMLFile(TestUtil.GmlFileCase.Simple, true, false);
+            
+            // 頂点数が 1 以上である ExteriorRing を検索し、最初に見つかったものをテスト対象とします。
+            this.exteriorRing = cityModel.RootCityObjects
+                .SelectMany(co => co.CityObjectDescendantsDFS)
+                .SelectMany(co => co.Geometries)
+                .SelectMany(geom => geom.Polygons)
+                .First(poly => poly.ExteriorRing.VertexCount > 0)
+                .ExteriorRing;
+        }
+        
+        [TestMethod]
+        public void ExteriorRing_Is_Not_Null()
+        {
+            Console.WriteLine(this.exteriorRing.Handle.ToInt64());
+            Assert.IsNotNull(this.exteriorRing);
+        }
+        
+        [TestMethod]
+        public void VerticesCount_Returns_Positive_Number()
+        {
+            int verticesCount = this.exteriorRing.VertexCount;
+            Console.WriteLine($"Vertices Count: {verticesCount}");
+            Assert.IsTrue(verticesCount > 0);
+        }
+
+        [TestMethod]
+        public void GetVertex_Returns_Non_Zero()
+        {
+            var vert = this.exteriorRing.GetVertex(this.exteriorRing.VertexCount - 1);
+            Console.WriteLine($"Vertex: {vert}");
+            Assert.IsTrue(vert.IsNotZero());
+        }
+    }
+}
---
+++ wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/CityGML/TextureCoordinatesTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PLATEAU.CityGML;
+
+namespace PLATEAU.Test.CityGML
+{
+
+    [TestClass]
+    public class TextureCoordinatesTests
+    {
+        private TextureCoordinates texCoords;
+        private LinearRing targetRing;
+
+        // 前準備
+        public TextureCoordinatesTests()
+        {
+            // 探索して最初に見つかった TextureCoordinates をテスト対象にします。
+            var cityModel = TestUtil.LoadTestGMLFile(TestUtil.GmlFileCase.Simple, true, false);
+            var texTarget = cityModel.RootCityObjects
+                .SelectMany(co => co.CityObjectDescendantsDFS)
+                .SelectMany(co => co.Geometries)
+                .SelectMany(geom => geom.GeometryDescendantsDFS)
+                .SelectMany(geom => geom.Polygons)
+                .Where(poly => poly.TextureTargetDefinitionsCount > 0)
+                .Select(poly => poly.GetTextureTargetDefinition(0))
+                .First(ttd => ttd.TexCoordinatesCount > 0);
+            this.texCoords = texTarget.GetCoordinate(0);
+
+            // texTarget のターゲットと同じIDを持つ LinearRing を探します。
+            string targetRingId = this.texCoords.TargetLinearRingId;
+            this.targetRing = cityModel.RootCityObjects
+                .SelectMany(co => co.CityObjectDescendantsDFS)
+                .SelectMany(co => co.Geometries)
+                .SelectMany(geom => geom.GeometryDescendantsDFS)
+                .SelectMany(geom => geom.Polygons)
+                .Select(poly => poly.ExteriorRing)
+                .First(ring => ring.ID == targetRingId);
+        }
+
+        [TestMethod]
+        public void Vec2CoordsCount_Is_Same_As_Linear_Ring_Vertices_Count()
+        {
+            int vec2CoordsCount = this.texCoords.Vec2CoordsCount;
+            int ringVerticesCount = this.targetRing.VertexCount;
+            Console.WriteLine($"Vec2CoordsCount = {vec2CoordsCount}");
+            Console.WriteLine($"RingVerticesCount = {ringVerticesCount}");
+            Assert.AreEqual(ringVerticesCount, vec2CoordsCount);
+        }
+
+        [TestMethod]
+        public void GetVec2Coord_Is_Not_Zero()
+        {
+            var vec2 = this.texCoords.GetVec2Coord(0);
+            Console.WriteLine($"GetVec2Coord(0) = {vec2}");
+            Assert.IsTrue(vec2.IsNotZero());
+        }
+
+        [TestMethod]
+        public void TargetLinearRingId_Returns_Not_Empty()
+        {
+            string id = this.texCoords.TargetLinearRingId;
+            Console.WriteLine($"Target Ring ID : {id}");
+            Assert.IsTrue(id.Length > 0);
+        }
+
+        [TestMethod]
+        public void IsRingTarget_Returns_True_For_Target()
+        {
+            Console.WriteLine($"target ring id: {this.targetRing.ID}");
+            bool actual = this.texCoords.IsRingTarget(this.targetRing);
+            Assert.IsTrue(actual);
+        }
+
+    }
+}
