db307b7e75c636caa12edfef210ac227baac19b1
linoal <1321932+linoal@users.noreply.github.com>
2022-08-17 18:02:06 +0900

    C#テスト追記

--- wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/CityGML/MeshMergerTests.cs
+++ wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/CityGML/MeshMergerTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PLATEAU.CityGML;
@@ -10,20 +9,11 @@ namespace PLATEAU.Test.CityGML
     [TestClass]
     public class MeshMergerTests
     {
-        private readonly MeshMerger meshMerger;
-
-        public MeshMergerTests()
-        {
-            this.meshMerger = new MeshMerger();
-        }
 
         [TestMethod]
         public void GridMerge_Returns_Polygons()
         {
-            var cityModel = TestUtil.LoadTestGMLFile(TestUtil.GmlFileCase.Simple);
-            var logger = new DllLogger();
-            logger.SetCallbacksToStdOut();
-            var polygons = this.meshMerger.GridMerge(cityModel, CityObjectType.COT_All, 5, 5, logger);
+            var polygons = LoadAndGridMerge();
             foreach (var poly in polygons)
             {
                 Console.WriteLine($"{poly.ID} : vertCount={poly.VertexCount}");
@@ -35,17 +25,30 @@ namespace PLATEAU.Test.CityGML
         [TestMethod]
         public void GridMerge_Composes_MultiTexture()
         {
-            var cityModel = TestUtil.LoadTestGMLFile(TestUtil.GmlFileCase.Simple);
-            var logger = new DllLogger();
-            logger.SetCallbacksToStdOut();
-            var polygons = this.meshMerger.GridMerge(cityModel, CityObjectType.COT_All, 5, 5, logger);
+            var polygons = LoadAndGridMerge();
             var multiTextures = polygons
                 .Select(poly => poly.GetMultiTexture())
                 .Where(multiTex => multiTex.Length > 0)
                 .ToArray();
             Assert.IsTrue(multiTextures.Length >= 3, "MultiTextureを含むポリゴンが3つ以上あります");
             Assert.IsFalse(string.IsNullOrEmpty(multiTextures[0][0].Texture.Url), "MultiTextureのURLにアクセスできます");
-    }
+        }
+
+        [TestMethod]
+        public void GridMerge_Size_Of_UV1_Equals_Num_Of_Vertices()
+        {
+            
+        }
+
+        private static PlateauPolygon[] LoadAndGridMerge()
+        {
+            var cityModel = TestUtil.LoadTestGMLFile(TestUtil.GmlFileCase.Simple);
+            var logger = new DllLogger();
+            logger.SetCallbacksToStdOut();
+            var meshMerger = new MeshMerger();
+            var polygons = meshMerger.GridMerge(cityModel, CityObjectType.COT_All, 5, 5, logger);
+            return polygons;
+        }
 
     }
 }
