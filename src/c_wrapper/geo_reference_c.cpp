de95b2eaab9a2feba977302d9420a336d3241fab
linoal <1321932+linoal@users.noreply.github.com>
2022-09-05 18:58:32 +0900

    GeoReferenceのC#ラッパーを作成

---
+++ src/c_wrapper/geo_reference_c.cpp
@@ -0,0 +1,36 @@
+#include "libplateau_c.h"
+#include <plateau/geometry/geo_reference.h>
+
+using namespace libplateau;
+using namespace plateau::geometry;
+extern "C" {
+    LIBPLATEAU_C_EXPORT APIResult LIBPLATEAU_C_API plateau_create_geo_reference(
+            GeoReference** out_geo_reference,
+            TVec3d reference_point,
+            float unit_scale,
+            CoordinateSystem coordinate_system,
+            int zone_id
+            ) {
+        API_TRY {
+            *out_geo_reference = new GeoReference(reference_point, unit_scale, coordinate_system, zone_id);
+            return APIResult::Success;
+        } API_CATCH
+        return APIResult::ErrorUnknown;
+    }
+
+    LIBPLATEAU_C_EXPORT APIResult LIBPLATEAU_C_API plateau_delete_geo_reference(
+            GeoReference* geo_reference
+            ){
+        API_TRY{
+            delete geo_reference;
+            return APIResult::Success;
+        }API_CATCH
+        return APIResult::ErrorUnknown;
+    }
+
+DLL_VALUE_FUNC(plateau_geo_reference_project,
+               GeoReference,
+               TVec3d,
+               handle->project(lat_lon),
+               , GeoCoordinate lat_lon)
+}
