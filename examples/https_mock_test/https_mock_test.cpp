354b32ca6655fb194def53d6a5dcf51b85d6ed78
Wataru Yasuda <wataru.robo@gmail.com>
2022-10-21 15:37:09 +0900

    add openssl

---
+++ examples/https_mock_test/https_mock_test.cpp
@@ -0,0 +1,22 @@
+﻿#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include <iostream>
+#include "../../3rdparty/cpp-httplib/httplib.h"
+#include "../../3rdparty/json/single_include/nlohmann/json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+int main(void) {
+    cout << "c++ Json client start !!" << endl;
+
+    httplib::Client cli("https://9tkm2n.deta.dev");
+    auto res = cli.Get("/api/sdk/files?codes=53392642,53392670");
+
+    if (res && res->status == 200) {
+        cout << "Response:" << endl;
+        auto jres = json::parse(res->body);
+        for (json::iterator it = jres.begin(); it != jres.end(); ++it) {
+            cout << it.key() << " : " << it.value() << endl;
+        }
+    }
+}
