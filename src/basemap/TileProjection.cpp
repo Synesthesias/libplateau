c8a5598b91965e65a5a18b92c206860f089bdaa5
和正 崎山 <kazsakinotwen@gmail.com>
2022-10-28 20:25:43 +0900

    API修正

--- src/basemap/TileProjection.cpp
+++ src/basemap/TileProjection.cpp
@@ -4,34 +4,34 @@
 
 namespace {
     int long2tileX(double longitude, int z) {
-        return (int)(floor((longitude + 180.0) / 360.0 * (1 << z)));
+        return static_cast<int>(floor((longitude + 180.0) / 360.0 * (1 << z)));
     }
 
     int lat2tileY(double latitude, int z) {
-        double latrad = latitude * M_PI / 180.0;
-        return (int)(floor((1.0 - asinh(tan(latrad)) / M_PI) / 2.0 * (1 << z)));
+        const double latrad = latitude * M_PI / 180.0;
+        return static_cast<int>(floor((1.0 - asinh(tan(latrad)) / M_PI) / 2.0 * (1 << z)));
     }
 
     double tileX2long(int x, int z) {
-        return x / (double)(1 << z) * 360.0 - 180;
+        return x / static_cast<double>(1 << z) * 360.0 - 180;
     }
 
     double tileY2lat(int y, int z) {
-        double n = M_PI - 2.0 * M_PI * y / (double)(1 << z);
+        const double n = M_PI - 2.0 * M_PI * y / static_cast<double>(1 << z);
         return 180.0 / M_PI * atan(0.5 * (exp(n) - exp(-n)));
     }
 }
 
-std::shared_ptr <std::vector<TileCoordinate>> TileProjection::getTileCoordinates(const plateau::geometry::Extent& extent, int zoomLevel) {
-    int minX = long2tileX(extent.min.longitude, zoomLevel);
-    int minY = lat2tileY(extent.max.latitude, zoomLevel);
-    int maxX = long2tileX(extent.max.longitude, zoomLevel);
-    int maxY = lat2tileY(extent.min.latitude, zoomLevel);
+std::shared_ptr<std::vector<TileCoordinate>> TileProjection::getTileCoordinates(const plateau::geometry::Extent& extent, int zoomLevel) {
+    const int min_x = long2tileX(extent.min.longitude, zoomLevel);
+    const int min_y = lat2tileY(extent.max.latitude, zoomLevel);
+    const int max_x = long2tileX(extent.max.longitude, zoomLevel);
+    const int max_y = lat2tileY(extent.min.latitude, zoomLevel);
 
     auto tileCoordinates = std::make_shared<std::vector<TileCoordinate>>();
-    for (int x = minX; x <= maxX; x++) {
-        for (int y = minY; y <= maxY; y++) {
-            TileCoordinate coordinate;
+    for (int x = min_x; x <= max_x; x++) {
+        for (int y = min_y; y <= max_y; y++) {
+            TileCoordinate coordinate{};
             coordinate.column = x;
             coordinate.row = y;
             coordinate.zoom_level = zoomLevel;
@@ -42,19 +42,19 @@ std::shared_ptr <std::vector<TileCoordinate>> TileProjection::getTileCoordinates
     return tileCoordinates;
 }
 
-std::shared_ptr <TileCoordinate> TileProjection::project(const plateau::geometry::GeoCoordinate& coordinate, int zoomLevel) {
-    auto tileCoordinate = std::make_shared<TileCoordinate>();
-    tileCoordinate->column = long2tileX(coordinate.longitude, zoomLevel);
-    tileCoordinate->row = lat2tileY(coordinate.latitude, zoomLevel);
-    tileCoordinate->zoom_level = zoomLevel;
+TileCoordinate TileProjection::project(const plateau::geometry::GeoCoordinate& coordinate, const int zoom_level) {
+    TileCoordinate tile_coordinate;
+    tile_coordinate.column = long2tileX(coordinate.longitude, zoom_level);
+    tile_coordinate.row = lat2tileY(coordinate.latitude, zoom_level);
+    tile_coordinate.zoom_level = zoom_level;
 
-    return tileCoordinate;
+    return tile_coordinate;
 }
 
-std::shared_ptr<plateau::geometry::GeoCoordinate> TileProjection::unproject(TileCoordinate& coordinate, int zoomLevel) {
-    auto geoCoordinate = std::make_shared<plateau::geometry::GeoCoordinate>();
-    geoCoordinate->latitude = tileY2lat(coordinate.row, zoomLevel);
-    geoCoordinate->longitude = tileX2long(coordinate.column, zoomLevel);
+plateau::geometry::GeoCoordinate TileProjection::unproject(const TileCoordinate& coordinate, const int zoom_level) {
+    plateau::geometry::GeoCoordinate geo_coordinate;
+    geo_coordinate.latitude = tileY2lat(coordinate.row, zoom_level);
+    geo_coordinate.longitude = tileX2long(coordinate.column, zoom_level);
 
-    return geoCoordinate;
+    return geo_coordinate;
 }
--- test/test_vector_tile.cpp
+++ test/test_vector_tile.cpp
@@ -14,19 +14,29 @@ TEST_F(VectorTileTest, VectorTileTest) {
     plateau::geometry::GeoCoordinate max(35.54136964, 139.78712557, 10000);
     plateau::geometry::Extent extent(min, max);
 
-    TileProjection tileProjection;
-    auto tileCoordinates = tileProjection.getTileCoordinates(extent, 15);
-
-    ASSERT_EQ(tileCoordinates->size(), 4);
-
-    VectorTileDownloader downloader("http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png");
     std::string destination = "./Basemap";
-    auto tile = downloader.download(destination.c_str(), tileCoordinates->front());
+    VectorTileDownloader downloader(destination, extent);
 
-    ASSERT_EQ(tile->coordinate.zoom_level, 15);
-    ASSERT_EQ(tile->coordinate.column, 29106);
-    ASSERT_EQ(tile->coordinate.row, 12918);
-    ASSERT_EQ(tile->image_path, destination + "\\15\\29106\\12918.png");
-    ASSERT_TRUE(std::filesystem::exists(tile->image_path));
+    ASSERT_EQ(downloader.getTileCount(), 4);
+    
+    auto coordinate = downloader.getTile(0);
+
+    ASSERT_EQ(coordinate.zoom_level, 15);
+    ASSERT_EQ(coordinate.column, 29106);
+    ASSERT_EQ(coordinate.row, 12918);
+
+    coordinate = downloader.getTile(1);
+
+    ASSERT_EQ(coordinate.zoom_level, 15);
+    ASSERT_EQ(coordinate.column, 29106);
+    ASSERT_EQ(coordinate.row, 12919);
+
+    auto tile = downloader.download(0);
+
+    ASSERT_EQ(tile.coordinate.zoom_level, 15);
+    ASSERT_EQ(tile.coordinate.column, 29106);
+    ASSERT_EQ(tile.coordinate.row, 12918);
+    ASSERT_EQ(tile.image_path, destination + "\\15\\29106\\12918.png");
+    ASSERT_TRUE(std::filesystem::exists(tile.image_path));
     std::filesystem::remove_all(destination);
 }
