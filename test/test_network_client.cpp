d74bf384586aa8fa6b8154b2089062d64cd2629c
Wataru Yasuda <wataru.robo@gmail.com>
2022-11-13 22:42:22 +0900

    should cause test error if return from Client class is NULL.

--- test/test_network_client.cpp
+++ test/test_network_client.cpp
@@ -18,6 +18,7 @@ namespace plateau::network {
 
     TEST_F(ClientTest, GetMetadata) {
         auto meta_data = client.getMetadata();
+        ASSERT_NE(meta_data.size(), 0);
         if (meta_data.size() != 0) {
             ASSERT_EQ(meta_data[0].id, "tokyo");
             ASSERT_EQ(meta_data[0].datasets[0].max_lod, 3);
@@ -26,6 +27,7 @@ namespace plateau::network {
 
     TEST_F(ClientTest, GetMeshCodes) {
         auto get_mesh_codes = client.getMeshCodes("23ku");
+        ASSERT_NE(get_mesh_codes->size(), 0);
         if (get_mesh_codes->size() != 0) {
             ASSERT_EQ(get_mesh_codes->at(0).get(), "53392642");
         }
@@ -36,6 +38,7 @@ namespace plateau::network {
         set_mesh_codes.push_back(plateau::udx::MeshCode("53392642"));
         set_mesh_codes.push_back(plateau::udx::MeshCode("53392670"));
         auto file_urls = client.getFiles(set_mesh_codes);
+        ASSERT_NE(file_urls->size(), 0);
         if (file_urls->size() != 0) {
             ASSERT_EQ(file_urls->begin()->second.at(0), "https://9tkm2n.deta.dev/13100_tokyo23-ku_2020_citygml_3_2_op/udx/bldg/53392642_bldg_6697_2_op.gml");
         }
