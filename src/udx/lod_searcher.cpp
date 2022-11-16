b024731a9c9999722686b9f06e8ef7f6282a6750
linoal <1321932+linoal@users.noreply.github.com>
2022-11-16 19:15:53 +0900

    LOD検索のチューニング

--- src/udx/lod_searcher.cpp
+++ src/udx/lod_searcher.cpp
@@ -1,18 +1,15 @@
 #include <plateau/udx/lod_searcher.h>
 #include <fstream>
 #include <stdexcept>
-#include <sstream>
-#include <iostream>
 
 using namespace plateau::udx;
 namespace fs = std::filesystem;
 
-namespace{
-    LodFlag searchLods(std::ifstream& ifs){
+namespace {
+    LodFlag searchLods(std::ifstream& ifs) {
         // 文字列検索で ":lod" にヒットした直後の数値をLODとします。
-        const static auto lod_mark = std::string(u8":lod");
-        const static auto lod_mark_size = lod_mark.size();
-
+        const static auto lod_mark = u8":lod";
+        const static auto lod_mark_size = strlen(lod_mark);
 
         auto lod_flag = LodFlag();
 
@@ -20,30 +17,32 @@ namespace{
         // 分割された1つを チャンク(chunk) と名付けます。
         constexpr int chunk_size = 16 * 1024;
         char chunk[chunk_size];
+        const char* const chunk_const = chunk;
 
         ifs.read(chunk, sizeof chunk);
         auto read_size = ifs.gcount();
-        while((!ifs.eof()) && read_size > 0){
+        while (!ifs.eof()) {
+            const char* const chunk_last = chunk_const + read_size - 1; // チャンク内の最後の文字のポインタ
             // チャンク内での文字列検索を始めます。 ":lod" を探します。
-            auto pos_ptr = strstr(chunk, lod_mark.c_str());
+            const char* pos_ptr = strstr(chunk, lod_mark);
 
             // ":lod" がヒットするたびに、その直後の数字をLODとみなして取得します。
-            while(pos_ptr){
+            while (pos_ptr) {
                 // ":lod" の次の文字の場所を指すポインタであり、LODの番号を指すことを期待します。
                 auto lod_num_ptr = std::min(
                         pos_ptr + lod_mark_size,
-                        chunk + read_size - 1
+                        chunk_last
                 );
 
                 // ":lod" の直後の数字を求めます。 数字は1桁であることが前提です。
                 int lod_num = *lod_num_ptr - u8'0';
-                if(0 <= lod_num && lod_num <= 9){
+                if (0 <= lod_num && lod_num <= 9) {
                     lod_flag.setFlag(lod_num);
                 }
 
                 // チャンク内の次の ":lod" を探します。
-                auto next_pos = std::min(pos_ptr + lod_mark_size, chunk + chunk_size - 1);
-                pos_ptr = strstr(next_pos, lod_mark.c_str());
+                auto next_pos = std::min(pos_ptr + lod_mark_size, chunk_last);
+                pos_ptr = strstr(next_pos, lod_mark);
             }
             // 次のチャンクに移ります。
             ifs.read(chunk, sizeof chunk);
@@ -56,18 +55,17 @@ namespace{
 }
 
 LodFlag LodSearcher::searchLodsInFile(const fs::path& file_path) {
-    auto ifs = std::ifstream (file_path);
-    if(!ifs){
+    auto ifs = std::ifstream(file_path);
+    if (!ifs) {
         throw std::runtime_error("Failed to read file.");
     }
     return searchLods(ifs);
 }
 
 
-
-namespace{
-    void throwIfOutOfRange(unsigned digit){
-        if(digit > LodFlag::max_lod_){
+namespace {
+    void throwIfOutOfRange(unsigned digit) {
+        if (digit > LodFlag::max_lod_) {
             throw std::range_error("argument digit is out of range.");
         }
     }
@@ -83,6 +81,6 @@ void LodFlag::unsetFlag(unsigned digit) {
     flags_ &= ~(1 << digit);
 }
 
-unsigned LodFlag::getFlag() const{
+unsigned LodFlag::getFlag() const {
     return flags_;
 }
