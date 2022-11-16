e52790b574e725aa7e222b03170ffb6c3577bd1f
linoal <1321932+linoal@users.noreply.github.com>
2022-11-16 16:17:46 +0900

    lod検索で、gmlをメモリに全部読むのではなく16kbごとに分割して読むことで高速化

--- src/udx/lod_searcher.cpp
+++ src/udx/lod_searcher.cpp
@@ -3,12 +3,13 @@
 #include <regex>
 #include <stdexcept>
 #include <sstream>
+#include <iostream>
 
 using namespace plateau::udx;
 namespace fs = std::filesystem;
 
 namespace{
-    LodFlag searchLodsInStr(const std::string& str){
+    LodFlag searchLods(std::ifstream& ifs){
         // 文字列検索で ":lod" にヒットした直後の数値をLODとします。
         const static auto lod_mark = std::string(u8":lod");
         const static auto lod_mark_size = lod_mark.size();
@@ -17,33 +18,50 @@ namespace{
         // 最大LOD値の10進数での桁数であり、正規表現で数値を検索する範囲に影響します。
         const static auto max_lod_digits_in_decimal = std::to_string(LodFlag::max_lod_).length();
 
-        const auto str_size = str.size();
-        // 検索を始めます。
-        auto lod_flag = LodFlag();
-        auto pos = str.find(lod_mark);
 
-        while(pos != std::string::npos){
-            // ":lod" の次の文字の場所
-            auto reg_search_begin_pos = std::min(
-                    pos + lod_mark_size,
-                    str_size
-                    );
-            // ":lod" の次から数字の桁数だけ正規表現で検索、その範囲の終了場所
-            auto reg_search_end_pos = std::min(
-                    reg_search_begin_pos + max_lod_digits_in_decimal + 1,
-                    str_size
-                    );
-            auto reg_search_begin_iter = str.begin() + (long long)reg_search_begin_pos;
-            auto reg_search_end_iter = str.begin() + (long long)reg_search_end_pos;
-            auto reg_results = std::smatch();
-            if(std::regex_search(reg_search_begin_iter, reg_search_end_iter, reg_results, reg_lod_num)){
-                unsigned lod = (unsigned)std::stoi(reg_results.str());
-                if(lod <= LodFlag::max_lod_){
-                    lod_flag.setFlag(lod);
+        auto lod_flag = LodFlag();
+
+        // GMLファイルを全部メモリに読み込むと重いので、16KBごとに分割して読み込みます。
+        // 分割された1つを チャンク(chunk) と名付けます。
+        constexpr int chunk_size = 16 * 1024;
+        char chunk[chunk_size];
+
+        ifs.read(chunk, sizeof chunk);
+        auto read_size = ifs.gcount();
+        while((!ifs.eof()) && read_size > 0){
+            // チャンク内での文字列検索を始めます。 ":lod" を探します。
+            auto pos_ptr = strstr(chunk, lod_mark.c_str());
+
+            // ":lod" がヒットするたびに、その直後の数字をLODとみなして取得します。
+            while(pos_ptr){
+                // ":lod" の次の文字の場所で、正規表現検索の開始地点
+                auto reg_search_begin_ptr = std::min(
+                        pos_ptr + lod_mark_size,
+                        chunk + read_size - 1
+                );
+                // ":lod" の次から数字の桁数だけ正規表現で検索、その範囲の終了場所
+                auto reg_search_end_ptr = std::min(
+                        reg_search_begin_ptr + max_lod_digits_in_decimal + 1,
+                        chunk + read_size - 1
+                );
+                auto reg_results = std::smatch();
+                auto reg_str = std::string(reg_search_begin_ptr, std::distance(reg_search_begin_ptr, reg_search_end_ptr));
+                if(std::regex_search(reg_str.cbegin(), reg_str.cend(), reg_results, reg_lod_num)){
+                    unsigned lod = (unsigned)std::stoi(reg_results.str());
+                    if(lod <= LodFlag::max_lod_){
+                        lod_flag.setFlag(lod);
+                    }
                 }
+                // チャンク内の次の ":lod" を探します。
+                auto next_pos = std::min(pos_ptr + lod_mark_size, chunk + chunk_size - 1);
+                pos_ptr = strstr(next_pos, lod_mark.c_str());
             }
-            pos = str.find(lod_mark, pos + lod_mark_size);
+            // 次のチャンクに移ります。
+            ifs.read(chunk, sizeof chunk);
+            read_size = ifs.gcount();
         }
+
+
         return lod_flag;
     }
 }
@@ -53,10 +71,10 @@ LodFlag LodSearcher::searchLodsInFile(const fs::path& file_path) {
     if(!ifs){
         throw std::runtime_error("Failed to read file.");
     }
-    std::ostringstream buffer;
-    buffer << ifs.rdbuf();
-    std::string file_body = buffer.str();
-    return searchLodsInStr(file_body);
+//    auto buffer = std::ostringstream();
+//    buffer << ifs.rdbuf();
+//    std::string file_body = buffer.str();
+    return searchLods(ifs);
 }
 
 
--- test/test_lod_searcher.cpp
+++ test/test_lod_searcher.cpp
@@ -7,7 +7,8 @@ namespace plateau::udx {
 
     class LodSearcherTest : public ::testing::Test {
     protected:
-        const std::string gml_path = u8"../data/udx/bldg/53392642_bldg_6697_op2.gml";
+//        const std::string gml_path = u8"../data/udx/bldg/53392642_bldg_6697_op2.gml";
+        const std::string gml_path = u8"../data/udx/bldg/53393690_bldg_6697_2_op.gml";
     };
 
     TEST_F(LodSearcherTest, searchLodsInFile_returns_gml_lods){ // NOLINT
