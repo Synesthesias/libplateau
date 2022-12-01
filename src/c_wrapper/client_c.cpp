53391a392a377774f7ecc886da6023ae07e45c06
linoal <1321932+linoal@users.noreply.github.com>
2022-12-01 13:00:16 +0900

    コードフォーマット

--- src/c_wrapper/client_c.cpp
+++ src/c_wrapper/client_c.cpp
@@ -1,9 +1,9 @@
 #include "libplateau_c.h"
 #include <plateau/network/client.h>
 
-extern "C"{
-    using namespace plateau::network;
-    using namespace libplateau;
+extern "C" {
+using namespace plateau::network;
+using namespace libplateau;
 
 DLL_CREATE_FUNC(plateau_create_client,
                 Client)
@@ -17,23 +17,23 @@ DLL_2_ARG_FUNC(plateau_client_get_metadata,
                arg_1->getMetadata(*arg_2))
 
 DLL_2_ARG_FUNC(plateau_client_set_api_server_url,
-              Client* const,
-              const char* const,
-              arg_1->setApiServerUrl(std::string(arg_2)))
+               Client* const,
+               const char* const,
+               arg_1->setApiServerUrl(std::string(arg_2)))
 
 DLL_STRING_VALUE_FUNC(plateau_client_get_api_server_url,
                       Client,
                       handle->getApiServerUrl())
 
-LIBPLATEAU_C_EXPORT libplateau::APIResult LIBPLATEAU_C_API plateau_client_download (
+LIBPLATEAU_C_EXPORT libplateau::APIResult LIBPLATEAU_C_API plateau_client_download(
         const Client* const client,
         const char* const destination_directory_utf8,
         const char* const url_utf8
-){
-    API_TRY{
+) {
+    API_TRY {
         client->download(destination_directory_utf8, url_utf8);
         return APIResult::Success;
-    }API_CATCH
+    } API_CATCH
     return APIResult::ErrorUnknown;
 }
 }
