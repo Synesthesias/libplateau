47dd7807f3a88f41d9d4ba86409ccab3d6d66299
linoal <1321932+linoal@users.noreply.github.com>
2022-12-08 18:01:39 +0900

    MetadataGroupの取得のC#テストを ServerDatasetAccessor から Clientに移動. C# test fail 3->2

--- wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/Dataset/ServerDatasetAccessorTest.cs
+++ wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/Dataset/ServerDatasetAccessorTest.cs
@@ -11,23 +11,6 @@ namespace PLATEAU.Test.Dataset
     [TestClass]
     public class ServerDatasetAccessorTest
     {
-        [TestMethod]
-        public void GetMetadataGroup_Returns_Array_Of_MetadataGroup()
-        {
-            var accessor = ServerDatasetAccessor.Create("");
-            var metadataGroups = accessor.GetMetadataGroup();
-            Assert.AreEqual(2, metadataGroups.Length);
-            var metadataGroup = metadataGroups.At(0);
-            Assert.AreEqual("tokyo", metadataGroup.ID);
-            Assert.AreEqual("東京都", metadataGroup.Title);
-            var datasets = metadataGroup.Datasets;
-            var dataset = datasets.At(0);
-            Assert.AreEqual("23ku", dataset.ID);
-            Assert.AreEqual("23区", dataset.Title);
-            Assert.AreEqual("xxxx", dataset.Description);
-            Assert.AreEqual(3, dataset.MaxLOD);
-            accessor.Dispose();
-        }
 
         // [TestMethod]
         // public void MetadataContainsFeatureTypes()
--- wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/Network/ClientTest.cs
+++ wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/Network/ClientTest.cs
@@ -7,13 +7,20 @@ namespace PLATEAU.Test.Network
     public class ClientTest
     {
         [TestMethod]
-        public void DatasetMetadataGroup()
+        public void GetDatasetMetadataGroup()
         {
             var client = Client.Create();
             client.Url = NetworkConfig.DefaultApiServerUrl;
-            var metadataGroup = client.GetDatasetMetadataGroup();
-            Assert.AreEqual("東京都", metadataGroup.At(0).Title);
-            Assert.AreEqual("23区", metadataGroup.At(0).Datasets.At(0).Title);
+            var metadataGroups = client.GetDatasetMetadataGroup();
+            Assert.AreEqual(2, metadataGroups.Length);
+            var group = metadataGroups.At(0);
+            Assert.AreEqual("tokyo", group.ID);
+            Assert.AreEqual("東京都", group.Title);
+            var dataset = group.Datasets.At(0);
+            Assert.AreEqual("23ku", dataset.ID);
+            Assert.AreEqual("23区", dataset.Title);
+            Assert.AreEqual("xxxx", dataset.Description);
+            Assert.AreEqual(3, dataset.MaxLOD);
             client.Dispose();
         }
 
