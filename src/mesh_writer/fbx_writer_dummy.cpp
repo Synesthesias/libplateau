1fb06f752d06e3a125d3207ed5dc38a21f38d12d
Wataru Yasuda <wataru.robo@gmail.com>
2023-01-10 12:16:03 +0900

    use fbx_writer_dummy for iOS

---
+++ src/mesh_writer/fbx_writer_dummy.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdexcept>
+#include <filesystem>
+#include <cassert>
+
+#include <plateau/mesh_writer/fbx_writer.h>
+
+//#include <fbxsdk.h>
+#include <set>
+
+namespace fs = std::filesystem;
+
+namespace plateau::meshWriter {
+    bool FbxWriter::write(const std::string& fbx_file_path, const plateau::polygonMesh::Model& model, const FbxWriteOptions& options) {
+        return false;
+    }
+}
