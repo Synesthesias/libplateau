61343ef6ce8a6fd421960d0c4d897bae15e0ca42
linoal <1321932+linoal@users.noreply.github.com>
2023-02-02 17:07:58 +0900

    デフォルトURLはC#からアクセスしないようにした。

--- src/c_wrapper/client_c.cpp
+++ src/c_wrapper/client_c.cpp
@@ -39,13 +39,6 @@ DLL_4_ARG_FUNC(plateau_client_download,
                std::string* const out_downloaded_path,
                *out_downloaded_path = client->download(destination_directory, url) )
 
-DLL_1_ARG_FUNC(plateau_client_get_default_url,
-               std::string* out_default_url,
-               *out_default_url = Client::getDefaultServerUrl())
-
-DLL_1_ARG_FUNC(plateau_client_get_default_api_token,
-               std::string* out_default_api_token,
-               *out_default_api_token = Client::getDefaultApiToken())
 
 DLL_1_ARG_FUNC(plateau_client_get_mock_server_url,
                std::string* out_mock_server_url,
--- wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/Network/NetworkConfigTests.cs
+++ wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/Network/NetworkConfigTests.cs
@@ -8,18 +8,6 @@ namespace PLATEAU.Test.Network
     {
 
         [TestMethod]
-        public void GetDefaultApiServerUrl()
-        {
-            Assert.AreEqual("https://api.plateau.reearth.io", NetworkConfig.DefaultApiServerUrl);
-        }
-
-        [TestMethod]
-        public void GetDefaultApiToken()
-        {
-            Assert.AreEqual("secret-56c66bcac0ab4724b86fc48309fe517a", NetworkConfig.DefaultApiToken);
-        }
-
-        [TestMethod]
         public void GetMockServerUrl()
         {
             Assert.AreEqual("https://plateau-api-mock-v2.deta.dev", NetworkConfig.MockServerUrl);
