e29ab0622befde5f8303dfd9a0ebf1775e891502
linoal <1321932+linoal@users.noreply.github.com>
2023-05-29 23:27:58 +0900

    GMLファイルの中身が少ない時に最大LODを正しく取得できないバグを修正　＆　ユニットテスト追加

--- src/dataset/lod_searcher.cpp
+++ src/dataset/lod_searcher.cpp
@@ -1,4 +1,4 @@
-#include "lod_searcher.h"
+#include <plateau/dataset/lod_searcher.h>
 #include <fstream>
 #include <stdexcept>
 #include <cstring>
@@ -26,51 +26,6 @@ namespace {
         }
     }
 
-    LodFlag searchLods(std::ifstream& ifs) {
-        // 注意:
-        // この関数は実行速度にこだわる必要があります。
-        // 用途はPLATEAUデータのインポートにおける範囲選択画面で、多くのGMLファイルについて利用可能なLODを検索します。
-
-        // 文字列検索で ":lod" にヒットした直後の数値をLODとします。
-        const static auto lod_pattern = u8":lod";
-        const static auto lod_pattern_size = strlen(lod_pattern);
-
-        auto lod_flag = LodFlag();
-
-        // GMLファイルを全部メモリに読み込むと重いので、16KBごとに分割して読み込みます。
-        // 分割された1つを チャンク(chunk) と名付けます。
-        constexpr int chunk_size = 16 * 1024;
-        char chunk[chunk_size];
-        const char* const chunk_const = chunk;
-
-        ifs.read(chunk, sizeof chunk);
-        auto read_size = ifs.gcount();
-        while (!ifs.eof()) {
-            const char* const chunk_last = chunk_const + read_size - 1; // チャンク内の最後の文字のポインタ
-            // チャンク内での文字列検索を始めます。 ":lod" を探します。
-            const char* found_ptr = strstr(chunk, lod_pattern);
-
-            // ":lod" がヒットするたびに、その直後の数字をLODとみなして取得します。
-            while (found_ptr) {
-                // ":lod"の直後が数字であれば、そのLODをセット
-                readCharAndSetLod(found_ptr, lod_pattern_size, 0, chunk_last, lod_flag);
-                // ":lod" の直後が ">" であれば、 ":lod>"の直後の数字のLODをセット (dem向け)
-                if(*getCharPtr(found_ptr, lod_pattern_size, 0, chunk_last) == u8'>'){
-                    readCharAndSetLod(found_ptr, lod_pattern_size, 1, chunk_last, lod_flag);
-                }
-
-                // チャンク内の次の ":lod" を探します。
-                auto next_pos = std::min(found_ptr + lod_pattern_size, chunk_last);
-                found_ptr = strstr(next_pos, lod_pattern);
-            }
-            // 次のチャンクに移ります。
-            ifs.read(chunk, sizeof chunk);
-            read_size = ifs.gcount();
-        }
-
-
-        return lod_flag;
-    }
 }
 
 LodFlag LodSearcher::searchLodsInFile(const fs::path& file_path) {
@@ -78,7 +33,53 @@ LodFlag LodSearcher::searchLodsInFile(const fs::path& file_path) {
     if (!ifs) {
         throw std::runtime_error("Failed to read file.");
     }
-    return searchLods(ifs);
+    return searchLodsInIstream(ifs);
+}
+
+LodFlag LodSearcher::searchLodsInIstream(std::istream& ifs) {
+    // 注意:
+    // この関数は実行速度にこだわる必要があります。
+    // 用途はPLATEAUデータのインポートにおける範囲選択画面で、多くのGMLファイルについて利用可能なLODを検索します。
+
+    // 文字列検索で ":lod" にヒットした直後の数値をLODとします。
+    const static auto lod_pattern = u8":lod";
+    const static auto lod_pattern_size = strlen(lod_pattern);
+
+    auto lod_flag = LodFlag();
+
+    // GMLファイルを全部メモリに読み込むと重いので、16KBごとに分割して読み込みます。
+    // 分割された1つを チャンク(chunk) と名付けます。
+    constexpr int chunk_size = 16 * 1024;
+    char chunk[chunk_size];
+    const char* const chunk_const = chunk;
+
+    ifs.read(chunk, sizeof chunk);
+    auto read_size = ifs.gcount();
+    do {
+        const char* const chunk_last = chunk_const + read_size - 1; // チャンク内の最後の文字のポインタ
+        // チャンク内での文字列検索を始めます。 ":lod" を探します。
+        const char* found_ptr = strstr(chunk, lod_pattern);
+
+        // ":lod" がヒットするたびに、その直後の数字をLODとみなして取得します。
+        while (found_ptr) {
+            // ":lod"の直後が数字であれば、そのLODをセット
+            readCharAndSetLod(found_ptr, lod_pattern_size, 0, chunk_last, lod_flag);
+            // ":lod" の直後が ">" であれば、 ":lod>"の直後の数字のLODをセット (dem向け)
+            if (*getCharPtr(found_ptr, lod_pattern_size, 0, chunk_last) == u8'>'){
+                readCharAndSetLod(found_ptr, lod_pattern_size, 1, chunk_last, lod_flag);
+            }
+
+            // チャンク内の次の ":lod" を探します。
+            auto next_pos = std::min(found_ptr + lod_pattern_size, chunk_last);
+            found_ptr = strstr(next_pos, lod_pattern);
+        }
+        // 次のチャンクに移ります。
+        ifs.read(chunk, sizeof chunk);
+        read_size = ifs.gcount();
+    } while (!ifs.eof());
+
+
+    return lod_flag;
 }
 
 
