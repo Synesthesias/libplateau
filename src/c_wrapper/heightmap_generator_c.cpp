6caa8bc012a206910bfa0def84836967c87487c9
sevendev <taka@7jigen.net>
2024-05-27 10:58:27 +0900

    Heightmap generate (#229)
    * class added.
    
    * c# fix
    
    * unity working version
    
    * temp commit
    
    * throw exceptions
    
    * unicode path 対応
    
    * UV計算
    
    * uv margin calc
    
    * uv default value
    
    * ubuntu build error 対応
    
    * Removed unused assets
    
    * macでgithub actionsが通らないのを修正中
    
    * 同上
    
    * 未使用処理削除
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 土壌
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * 同上
    
    * convolution filter 平滑化処理追加
    
    * comment added
    
    * Unity Terrain 用変更
    
    * UTF8に
    
    * log削除
    
    * ignore .editorconfig
    
    * commentd out cs unit test
    
    * revert unit test comment
    
    * Cwrapper : TVec2d -> TVec3dに変更
    
    * LIBPLATEAU_EXPORT  added.
    
    * Tile分割による高速化
    
    * CIが落ちるのを修正中
    
    * 同上
    
    * vertice -> vertex
    
    * Local変数小文字に
    
    * 変数全て小文字に
    
    ---------
    
    Co-authored-by: Linoal <1321932+linoal@users.noreply.github.com>

---
+++ src/c_wrapper/heightmap_generator_c.cpp
@@ -0,0 +1,98 @@
+#include <plateau/texture/heightmap_generator.h>
+#include "libplateau_c.h"
+#include <plateau/polygon_mesh/mesh.h>
+
+using namespace citygml;
+using namespace libplateau;
+using namespace plateau::polygonMesh;
+using namespace plateau::texture;
+using namespace plateau::geometry;
+
+extern "C" {
+    // 注意 : out_heightmap_dataはコピー終了後にrelease_heightmap_dataを呼んで削除する必要あり
+    LIBPLATEAU_C_EXPORT APIResult LIBPLATEAU_C_API heightmap_generator_generate_from_mesh(
+            Mesh* const src_mesh,
+            size_t TextureWidth,
+            size_t TextureHeight,
+            TVec3d margin,
+            CoordinateSystem coordinate,
+            TVec3d* outMin,
+            TVec3d* outMax,
+            TVec2f* outUVMin,
+            TVec2f* outUVMax,
+            const uint16_t** out_heightmap_data,
+            size_t* dataSize
+    ) {
+        API_TRY{
+            HeightmapGenerator generator;
+            const auto& vec = generator.generateFromMesh(*src_mesh, TextureWidth, TextureHeight, TVec2d(margin.x, margin.y) , coordinate, *outMin, *outMax, *outUVMin, *outUVMax);
+            uint16_t* heightmap_data = new uint16_t[vec.size()];
+            memcpy(heightmap_data, vec.data(), sizeof(uint16_t) * vec.size());
+            *out_heightmap_data = heightmap_data;
+            *dataSize = vec.size();
+            return APIResult::Success;
+        } 
+        API_CATCH;
+        return APIResult::ErrorUnknown;
+    }
+
+    LIBPLATEAU_C_EXPORT APIResult LIBPLATEAU_C_API heightmap_save_png_file(
+            const char* filename, size_t width, size_t height, uint16_t* data
+    ) {
+        API_TRY{
+            HeightmapGenerator::savePngFile(std::string(filename), width, height, data);
+            return APIResult::Success;
+        } API_CATCH;
+        return APIResult::ErrorUnknown;
+    }
+
+    // 注意 : out_heightmap_dataはコピー終了後にrelease_heightmap_dataを呼んで削除する必要あり
+    LIBPLATEAU_C_EXPORT APIResult LIBPLATEAU_C_API heightmap_read_png_file(
+      const char* filename, const size_t width, const size_t height, const uint16_t** out_heightmap_data, size_t* dataSize
+    ) {
+        API_TRY{
+            const auto& vec = HeightmapGenerator::readPngFile(std::string(filename), width, height);
+            uint16_t* heightmap_data = new uint16_t[vec.size()];
+            memcpy(heightmap_data, vec.data(), sizeof(uint16_t) * vec.size());
+            *out_heightmap_data = heightmap_data;
+            *dataSize = vec.size();
+            return APIResult::Success;
+        } API_CATCH;
+        return APIResult::ErrorUnknown;
+    }
+
+    LIBPLATEAU_C_EXPORT APIResult LIBPLATEAU_C_API heightmap_save_raw_file(
+        const char* filename, size_t width, size_t height, uint16_t* data
+    ) {
+        API_TRY{
+            HeightmapGenerator::saveRawFile(std::string(filename), width, height, data);
+            return APIResult::Success;
+        } API_CATCH;
+        return APIResult::ErrorUnknown;
+    }
+
+    // 注意 : out_heightmap_dataはコピー終了後にrelease_heightmap_dataを呼んで削除する必要あり
+    LIBPLATEAU_C_EXPORT APIResult LIBPLATEAU_C_API heightmap_read_raw_file(
+    const char* filename, size_t width, size_t height, const uint16_t** out_heightmap_data, size_t* dataSize
+    ) {
+        API_TRY{
+            const auto & vec = HeightmapGenerator::readRawFile(std::string(filename), width, height);
+            uint16_t* heightmap_data = new uint16_t[vec.size()];
+            memcpy(heightmap_data, vec.data(), sizeof(uint16_t) * vec.size());
+            *out_heightmap_data = heightmap_data;
+            *dataSize = vec.size();
+            return APIResult::Success;
+            } API_CATCH;
+            return APIResult::ErrorUnknown;
+    }
+
+    // heightmap_data配列削除
+    LIBPLATEAU_C_EXPORT APIResult LIBPLATEAU_C_API  release_heightmap_data(uint16_t* heightmap_data) {
+        API_TRY{
+            delete[] heightmap_data;
+        return APIResult::Success;
+        } API_CATCH;
+        return APIResult::ErrorUnknown;
+    }
+
+}
---
+++ wrappers/csharp/LibPLATEAU.NET/CSharpPLATEAU.Test/Texture/HeightmapGeneratorTest.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PLATEAU.Interop;
+using PLATEAU.Native;
+using PLATEAU.PolygonMesh;
+using PLATEAU.Texture;
+using System;
+using System.IO;
+
+namespace PLATEAU.Test.Texture
+{
+
+    [TestClass]
+    public class HeightmapGeneratorTest
+    {
+        public string ImageDirPath = "data/日本語パステスト/test_images";
+
+        [TestMethod]
+        public void ConvertTo2DFloatArrayTest()
+        {
+            ushort[] data = new ushort[4] { 65535, 0, 0, 65535 };
+            float[,] outData = HeightmapGenerator.ConvertTo2DFloatArray(data, 2, 2);
+            Assert.AreEqual(0f, outData[0, 0]);
+            Assert.AreEqual(1f, outData[0, 1]);
+            Assert.AreEqual(1f, outData[1, 0]);
+            Assert.AreEqual(0f, outData[1, 1]);
+        }
+        
+        [TestMethod]
+        public void TestGenerateFromMesh()
+        {
+            SimpleMeshInfo(out var vertices, out var indices, out var uv1, out var uv4, out var subMeshes);
+            var mesh = PLATEAU.PolygonMesh.Mesh.Create(vertices, indices, uv1, uv4, subMeshes);
+
+            int textureWidth = 513;
+            int textureHeight = 513;
+            PlateauVector2d margin = new PlateauVector2d(0, 0);
+
+            HeightmapGenerator gen = new HeightmapGenerator();
+            gen.GenerateFromMesh(mesh, textureWidth, textureHeight, margin, out var min, out var max, out var minUV, out var maxUV, out var outData);
+            Assert.AreEqual(textureWidth * textureHeight, outData.Length);
+        }
+
+        [TestMethod]
+        public void TestReadWritePng()
+        {
+            var filePath = Path.Combine(ImageDirPath, "test_heightmap.png");
+            Assert.IsTrue(Directory.Exists(ImageDirPath), "Directory Exists");
+            Assert.IsTrue(File.Exists(filePath), "File Exists");
+
+            //Read
+            int textureWidth = 513;
+            int textureHeight = 513;
+            HeightmapGenerator.ReadPngFile(filePath, textureWidth, textureHeight, out var imageData);
+            Assert.AreEqual(textureWidth * textureHeight, imageData.Length);
+
+            //Write
+            HeightmapGenerator.SavePngFile(Path.Combine(ImageDirPath, "test_heightmap_saved.png"), textureWidth, textureHeight, imageData);
+        }
+
+        [TestMethod]
+        public void TestReadWriteRaw()
+        {
+            var filePath = Path.Combine(ImageDirPath, "test_heightmap.raw");
+            Assert.IsTrue(Directory.Exists(ImageDirPath), "Directory Exists");
+            Assert.IsTrue(File.Exists(filePath), "File Exists");
+
+            //Read
+            int textureWidth = 513;
+            int textureHeight = 513;
+            HeightmapGenerator.ReadRawFile(filePath, textureWidth, textureHeight, out var imageData);
+            Assert.AreEqual(textureWidth * textureHeight, imageData.Length);
+
+            //Write
+            HeightmapGenerator.SaveRawFile(Path.Combine(ImageDirPath, "test_heightmap_saved.raw"), textureWidth, textureHeight, imageData);
+        }
+
+
+        private static PLATEAU.PolygonMesh.Mesh CreateSimpleMesh()
+        {
+            SimpleMeshInfo(out var vertices, out var indices, out var uv1, out var uv4, out var subMeshes);
+            var mesh = PLATEAU.PolygonMesh.Mesh.Create(vertices, indices, uv1, uv4, subMeshes);
+            return mesh;
+        }
+
+        private static void SimpleMeshInfo(out PlateauVector3d[] vertices, out uint[] indices,
+            out PlateauVector2f[] uv1, out PlateauVector2f[] uv4, out SubMesh[] subMeshes)
+        {
+            vertices = new[]
+            {
+                new PlateauVector3d(0, 0, 0),
+                new PlateauVector3d(0, 100, 10),
+                new PlateauVector3d(100, 100, 30),
+                new PlateauVector3d(100, 0, 10),
+            };
+            indices = new uint[]
+            {
+                0, 1, 2, 3, 2, 0
+            };
+            uv1 = new[]
+            {
+                new PlateauVector2f(1.1f, 1.2f),
+                new PlateauVector2f(2.1f, 2.2f),
+                new PlateauVector2f(3.1f, 3.2f)
+            };
+            uv4 = new[]
+            {
+                new PlateauVector2f(0, 0),
+                new PlateauVector2f(0, 0),
+                new PlateauVector2f(0, 0),
+                new PlateauVector2f(0, 0)
+            };
+            subMeshes = new[]
+            {
+                SubMesh.Create(0, 2, "testTexturePath.png")
+            };
+        }
+        
+    }
+}
