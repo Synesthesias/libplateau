625bfc98fec9ce115a6b483dff3581f2759b37dd
sevendev <taka@7jigen.net>
2025-08-21 10:47:24 +0900

    nitpick対応

--- test/test_tile_extractor.cpp
+++ test/test_tile_extractor.cpp
@@ -1,9 +1,7 @@
 #include "gtest/gtest.h"
 #include "citygml/citymodel.h"
 #include "citygml/citygml.h"
-#include "plateau/dataset/grid_code.h"
 #include <plateau/polygon_mesh/tile_extractor.h>
-#include <plateau/dataset/gml_file.h>
 
 using namespace citygml;
 using namespace plateau::geometry;
@@ -23,6 +21,7 @@ namespace plateau::polygonMesh {
 
         const std::string gml_path = u8"../data/日本語パステスト/tile_test/tran/52353549_tran_6697_op.gml";
         const std::shared_ptr<const CityModel> city_model = load(gml_path, params_);
+        ASSERT_TRUE(city_model);
 
         // zoomLevel 11の場合のグリッド分割
         MeshExtractOptions mesh_extract_options = MeshExtractOptions();
@@ -35,25 +34,33 @@ namespace plateau::polygonMesh {
         const auto min = city_model->getEnvelope().getLowerBound();
         const auto max = city_model->getEnvelope().getUpperBound();
 
-        std::vector extents = {
+        std::vector<Extent> extents = {
             Extent(GeoCoordinate(min.x, min.y, min.z), GeoCoordinate(max.x, max.y, max.z))
         };
 
         const auto model = TileExtractor::extractWithGrid(*city_model, mesh_extract_options, extents);
 
-		ASSERT_EQ(model->getRootNodeCount(), 4); // 2x2のグリッドなので最大4つのGRIDノードがあるはず（オブジェクトがなければ少なくなる）
+		ASSERT_EQ(model->getRootNodeCount(), 4); // 2x2のグリッドなので最大4つのGRIDノードがあるはず（オブジェクトがなければ少なくなる）Test用の読込GMLは4分割される
 
 		// グリッドノードの名前を確認
         const auto& grid_node = model->getRootNodeAt(0);
         const auto& grid_name = grid_node.getName();
         ASSERT_EQ(grid_name.substr(0, 4), "GRID");
 
+        for (size_t i = 0; i < model->getRootNodeCount(); ++i) {
+            const auto& node = model->getRootNodeAt(i);
+            const auto& name = node.getName();
+            ASSERT_EQ(name.substr(0, 4), "GRID");
+        }
+
 		// LODノードの名前を確認
+        ASSERT_GT(grid_node.getChildCount(), 0);
         const auto& lod_node = grid_node.getChildAt(0);
         const auto& lod_name = lod_node.getName();
         ASSERT_EQ(lod_name.substr(0, 3), "LOD");
 
 		// グリッドノードの子ノードの名前を確認
+        ASSERT_GT(lod_node.getChildCount(), 0);
         const auto& first_model_node = lod_node.getChildAt(0);
         const auto& first_model_node_name = first_model_node.getName();
         ASSERT_EQ(first_model_node_name.substr(0, 5), "group");
@@ -73,13 +80,15 @@ namespace plateau::polygonMesh {
         const std::string gml_path2 = u8"../data/日本語パステスト/tile_test/tran/52353549_tran_6697_op.gml";
         const std::shared_ptr<const CityModel> city_model1 = load(gml_path1, params_);
         const std::shared_ptr<const CityModel> city_model2 = load(gml_path2, params_);
+        ASSERT_TRUE(city_model1);
+        ASSERT_TRUE(city_model2);
 
         const auto min1 = city_model1->getEnvelope().getLowerBound();
         const auto max1 = city_model1->getEnvelope().getUpperBound();
         const auto min2 = city_model2->getEnvelope().getLowerBound();
         const auto max2 = city_model2->getEnvelope().getUpperBound();
 
-        std::vector extents = {
+        std::vector<Extent> extents = {
             Extent(GeoCoordinate(min1.x, min1.y, min1.z), GeoCoordinate(max1.x, max1.y, max1.z)),
             Extent(GeoCoordinate(min2.x, min2.y, min2.z), GeoCoordinate(max2.x, max2.y, max2.z)),
         };
@@ -92,11 +101,12 @@ namespace plateau::polygonMesh {
         auto model = TileExtractor::extractWithCombine(city_models, mesh_extract_options, extents);
 
         // LODノードの名前を確認
+        ASSERT_GT(model->getRootNodeCount(), 0);
         const auto& lod_node = model->getRootNodeAt(0);
         const auto& lod_name = lod_node.getName();
         ASSERT_EQ(lod_name.substr(0, 3), "LOD");
 
-        ASSERT_EQ(lod_node.getChildCount(), 1); // 結合なので1つの子ノードがあるはず
+        ASSERT_EQ(lod_node.getChildCount(), 1); // 結合されるので子ノードは１つ
 
         // 子ノードの名前を確認
         const auto& first_model_node = lod_node.getChildAt(0);
